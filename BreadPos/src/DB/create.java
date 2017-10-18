69e5980af5fd7086ce6c45859d37d0ccde69b08e
togla <togla@Saehim-xinli>
2017-10-18 17:10:01 +0900

    DB create

---
+++ BreadPos/src/DB/create.java
@@ -0,0 +1,33 @@
+package DB;
+
+public class create {
+	/*
+	 CREATE TABLE office(
+    office_num number not null primary key,
+    office_ID varchar2(15) not null,
+    office_name varchar2(15) not null,
+    office_pass varchar2(20) not null
+);
+
+insert into office values (100, 'sm100', 'ÁøÈï¿øµ¿Á¡', '1234');
+
+CREATE TABLE bread_info (
+    bread_name varchar2(15) not null PRIMARY KEY,
+    bread_price number not null,
+    bread_kind varchar2(15) not null
+);
+
+CREATE TABLE member_info (
+    member_phone varchar2(15) not null,
+    member_name varchar2(15) not null,
+    member_money number DEFAULT 0
+);
+
+CREATE TABLE daily_import(
+    daily_day number not null PRIMARY KEY,
+    daily_money number not null,
+    daily_officeNum number not null
+);
+	 */
+
+}
