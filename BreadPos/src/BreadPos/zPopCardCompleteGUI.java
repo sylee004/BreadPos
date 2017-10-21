8d6f17134ca7ba3ad5f2e8b85fef439885be8165
LEE SEYOUNG <LEE SEYOUNG@DESKTOP-71O9UVV>
2017-10-21 17:26:50 +0900

    .

--- BreadPos/src/BreadPos/zPopCardCompleteGUI.java
+++ BreadPos/src/BreadPos/zPopCardCompleteGUI.java
@@ -71,8 +71,7 @@ public class zPopCardCompleteGUI {
 		panel.addMouseListener(new MouseAdapter() {
 
 			public void mouseClicked(MouseEvent arg0) {
-				ManagementGUI m = new ManagementGUI();
-				m.main(null);
+			
 				frame.dispose();
 
 			}
